93e6558cc5083ba5b018f0c85a25a5e5ac0f9d0c
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-02-20 21:21:15 +0700

    Two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './course/course.component';
+import { CoursesService } from './courses.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,10 @@ import { CoursesComponent } from './course/course.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [CoursesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

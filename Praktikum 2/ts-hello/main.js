b821dd561911c6da094073b50b588d46665c0d5c
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-02-27 16:30:42 +0700

    Moving a file

---
+++ Praktikum 2/ts-hello/main.js
@@ -0,0 +1,51 @@
+var drawPoint = function (point) {
+    // ....
+};
+drawPoint({
+    x: 1,
+    y: 3
+});
+// class Pointku1{
+//     a: number;
+//     b: number;
+//     draw(){
+//         console.log('X' + this.a + ', Y' + this.b);
+//     }
+//     getDistance(another: Pointku1){
+//     }
+// }
+// let coins: Pointku1
+// coins.draw();
+// class Pointku{
+//     a: number;
+//     b: number;
+//     draw(){
+//     }
+//     getDistance(another: Pointku){
+//     }
+// }
+// let log = function pesan(){
+//     console.log(pesan);
+// }
+// let doLog = (pesan) => {
+//     console.log(pesan);
+// }
+// let doLog1 = (pesan) => console.log(pesan);
+// let pesan;
+// pesan = 'abc';
+// let percobaan = (<string>pesan).endsWith('c');
+// let alternative = (pesan as string).endsWith('c');
+// let count = 5;
+// count = 'a';
+// function doSomething(){
+//     for (let i = 0; i < 5; i++){
+//         console.log(i);
+//     }
+//     console.log('Finally: ' + i);
+// }
+// doSomething();
+// function log(message){
+//     console.log(message);
+// }
+// let message = 'Hello World';
+// log(message);
---
+++ Praktikum 3/src/app/course/course.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { CoursesService } from '../courses.service';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
+})
+export class CoursesComponent implements OnInit {
+  
+  title = 'Belajar Angular';
+  Courses;
+
+  binding = 'property-binding';
+  imageUrl = 'http://lorempixel.com/400/200';
+  colSpan = 2;
+  isActive = true;
+
+  constructor(private service: CoursesService) {
+    this.Courses = service.getCourses();
+  }
+
+  ngOnInit() {
+  }
+
+}

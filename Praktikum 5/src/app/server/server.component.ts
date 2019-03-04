4b4259548fffbe1b1b691f9ff7de06600465c98f
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-03-04 20:46:09 +0700

    Component Styles

--- Praktikum 5/src/app/server/server.component.ts
+++ Praktikum 5/src/app/server/server.component.ts
@@ -2,14 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  template: `<h1> hello {{name}} </h1>`,
+  // styleUrls: ['./server.component.css']
+  styles:[`
+  h1 {
+    color: dodgerblue;
+  }`]
 })
 export class ServerComponent implements OnInit {
 
+  name: string = "angular"
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {  
   }
 
 }

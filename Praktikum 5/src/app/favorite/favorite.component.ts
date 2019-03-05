c8dbce06e84b55ee449b07f6f944c375d7748ff2
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-03-05 17:30:11 +0700

    Event Emitter

--- Praktikum 5/src/app/favorite/favorite.component.ts
+++ Praktikum 5/src/app/favorite/favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-favorite',
@@ -12,6 +12,8 @@ export class FavoriteComponent implements OnInit {
   // isFavorite: boolean;
   @Input('aliasFavorite') isSelected: boolean;
 
+  @Output() change = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
@@ -23,5 +25,6 @@ export class FavoriteComponent implements OnInit {
 
   onClickAlias(){
     this.isSelected =! this.isSelected;
+    this.change.emit();
   }
 }

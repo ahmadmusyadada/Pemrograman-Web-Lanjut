af803eade9eb7edc88a3b3479197c5bc005ea91c
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-03-13 15:49:53 +0700

    Custom Directive

--- Praktikum 6/src/app/input-format.directive.ts
+++ Praktikum 6/src/app/input-format.directive.ts
@@ -1,18 +1,26 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener, ElementRef, Input } from '@angular/core';
+import { format } from 'url';
 
 @Directive({
   selector: '[appInputFormat]'
 })
 
 export class InputFormatDirective {
+  // @Input('format')format;
+  @Input('appInputFormat')format;
 
-  @HostListener('focus') onfocus(){
-    console.log("onFocus");
-  }
-
+  // @HostListener('focus') onfocus(){
+  //   console.log("onFocus");
+  // }
+  
+  constructor(private el: ElementRef) { }
   @HostListener('blur') onblur(){
-    console.log("onBlur");
-  }
-  constructor() { }
+    // console.log("onBlur");
 
+    let value: string = this.el.nativeElement.value;
+    if(this.format == 'lowervase')
+      this.el.nativeElement.value = value.toLowerCase();
+    else
+      this.el.nativeElement.value = value.toUpperCase();
+  }
 }

f93c8d416e4033775cdf6ae6914f0c3c64428b99
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-03-13 12:27:15 +0700

    Bab 5

---
+++ Praktikum 6/src/app/server/server.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  // template: `<h1> hello {{name}} </h1>`,
+  styleUrls: ['./server.component.css']
+  // styles:[`
+  // h1 {
+    // color: dodgerblue;
+  // }`]
+})
+export class ServerComponent implements OnInit {
+
+  // name: string = "angular"
+  constructor() { }
+
+  ngOnInit() {  
+  }
+
+}

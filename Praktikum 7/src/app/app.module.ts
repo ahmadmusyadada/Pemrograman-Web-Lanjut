2d66aaf8291fa411b1b7d247fefecb2eeba45168
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-03-20 20:31:22 +0700

    ngModel

--- Praktikum 7/src/app/app.module.ts
+++ Praktikum 7/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 
@@ -11,13 +12,14 @@ import { TemplateDrivenComponent } from './template-driven/template-driven.compo
   declarations: [
     AppComponent,
     ReactiveFormComponent,
-    TemplateDrivenComponent
+    TemplateDrivenComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Praktikum 7/src/app/contact/contact.component.ts
+++ Praktikum 7/src/app/contact/contact.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
+
 export class ContactComponent implements OnInit {
+  @Input() nama: string;
+  @Input() comment: string;
 
   constructor() { }
 

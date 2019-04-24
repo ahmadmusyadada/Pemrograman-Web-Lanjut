b03247bd3164d9cf434d141b0a9f489be007558c
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-04-24 13:42:37 +0700

    Soal 2

--- Praktikum 9/src/app/app.module.ts
+++ Praktikum 9/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
-import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { PostsComponent } from './posts/posts.component';
@@ -13,17 +13,16 @@ import { PostsComponent } from './posts/posts.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ReactiveFormComponent,
-    TemplateDrivenComponent,
-    ContactComponent,
-    SignupFormComponent,
+    // TemplateDrivenComponent,
+    // ContactComponent,
+    // SignupFormComponent,
     PostsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    // HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Praktikum 9/src/app/posts/posts.component.ts
+++ Praktikum 9/src/app/posts/posts.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent implements OnInit {
+export class PostsComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(http: Http) { }
 
 }
